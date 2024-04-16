056b0696dcb4b567811f442512271aaad04d2899
alleappio <allevanny@gmail.com>
2024-04-16 21:47:20 +0200

    added gitignore

--- game/build/CMakeFiles/game.dir/src/snake.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/game.dir/src/snake.cpp.o: \
- /home/appio/prove_raylib/game/src/snake.cpp /usr/include/stdc-predef.h \
- /home/appio/prove_raylib/game/include/snake.hpp \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
